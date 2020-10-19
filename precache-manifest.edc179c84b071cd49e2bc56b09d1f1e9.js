a6442f74614d66bb8e0763e1db420a035ce7c46a
cumalikarakoc <cumalikarakoc783@hotmail.com>
2020-10-19 23:12:15 +0200

    Updates

---
+++ precache-manifest.edc179c84b071cd49e2bc56b09d1f1e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1f5470991ebc7edc12f4036a48d0d5",
+    "url": "/carcassonne-scoreboard/index.html"
+  },
+  {
+    "revision": "d350826a3b36ba8b6200",
+    "url": "/carcassonne-scoreboard/static/css/main.fab446ce.chunk.css"
+  },
+  {
+    "revision": "290e4b147ec2ea42b592",
+    "url": "/carcassonne-scoreboard/static/js/2.69f096f3.chunk.js"
+  },
+  {
+    "revision": "98d0875b14651dd4cd2fc359964e844d",
+    "url": "/carcassonne-scoreboard/static/js/2.69f096f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d350826a3b36ba8b6200",
+    "url": "/carcassonne-scoreboard/static/js/main.909c05a2.chunk.js"
+  },
+  {
+    "revision": "c380a64ab2817b9b21bb",
+    "url": "/carcassonne-scoreboard/static/js/runtime-main.7fbbb93c.js"
+  },
+  {
+    "revision": "eafadc4b3e88a1119b4fbe739f1ffaca",
+    "url": "/carcassonne-scoreboard/static/media/black_robber.eafadc4b.png"
+  },
+  {
+    "revision": "4971e69e5e689ba5696bb776b7053407",
+    "url": "/carcassonne-scoreboard/static/media/blue_robber.4971e69e.png"
+  },
+  {
+    "revision": "3489325673c0f8c62ce023848cdf0ca8",
+    "url": "/carcassonne-scoreboard/static/media/green_player.34893256.png"
+  },
+  {
+    "revision": "b90ecfeb58374f6afb3f8a7bcae3c1c9",
+    "url": "/carcassonne-scoreboard/static/media/green_robber.b90ecfeb.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/carcassonne-scoreboard/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "048406045766e40f4101f38172392416",
+    "url": "/carcassonne-scoreboard/static/media/pink_robber.04840604.png"
+  },
+  {
+    "revision": "cbe17d0f35b3d3290581f18947e85ebc",
+    "url": "/carcassonne-scoreboard/static/media/red_player.cbe17d0f.png"
+  },
+  {
+    "revision": "4e4771ff4fe2c8a8c1040bd63bea298b",
+    "url": "/carcassonne-scoreboard/static/media/red_robber.4e4771ff.png"
+  },
+  {
+    "revision": "b2e1b188232711e7a2e7429b5e6c001e",
+    "url": "/carcassonne-scoreboard/static/media/yellow_robber.b2e1b188.png"
+  }
+]);
